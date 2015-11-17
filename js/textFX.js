43ec42aa9c0f79cc57278a129a238e230a61e789
iap <al@alswebdesigns.net>
2015-11-17 13:53:24 -0800

    Added test div to split into spans. Started JS code to do it, but not working yet.

--- js/textFX.js
+++ js/textFX.js
@@ -1,6 +1,7 @@
 var pXY = document.getElementById("pXY");
 var spinEs = document.getElementsByClassName("spin");
 
+/*
 var spinR = [];
 
 for (var ii = spinEs.length; ii--;) {
@@ -8,45 +9,38 @@ for (var ii = spinEs.length; ii--;) {
   var r = e.getBoundingClientRect(); // Rectangle
   console.log("spinEs["+ii+"] at [top="+r.top+", bot="+r.bottom+", left="+r.left+", right="+r.right+"]  value=" + e);
   spinR.push(r);
-}
-
-/*function getOffset(el) {
-  var x = 0;
-  var y = 0;
-  while ( el && (! isNaN(el.offsetLeft)) && (! isNaN(el.offsetTop)) ) {
-      x += el.offsetLeft - el.scrollLeft;
-      y += el.offsetTop  - el.scrollTop;
-      el = el.offsetParent;
-  }
-  return { top: y, left: x };
-}
-
-var x = getOffset( document.getElementById('tt') ).left;
-var y = getOffset( document.getElementById('tt') ).top;
-console.log(x, y);*/
+}*/
 
 /* Check to see which elements' bounding boxes contain the point x,y.
    Used for "hit detection" in the animation.
+   IN   E   Element to "split"
+   OUT  Array of span elements that correspond to each word inside the input element
+*/
+function splitSpans(el) {
+  var Espans = []; // Array containing spans after splitting el
+  spans = el.textContent;
+  console.log("spans = " + spans);
+/*spans.push(e);*/
+  return Espans;
+}
 
+/* Check to see which elements' bounding boxes contain the point x,y.
+   Used for "hit detection" in the animation.
    IN   E    Array of DOM elements
         x,y  Query point's coords
-
-   OUT  List of elements that contain the query point
+   OUT  Array of elements that contain the query point
 */
 function elementsAtPos(E, x, y) {
   var Ehits = []; // List of elements that contain P
-//console.log("here. E.length = "+E.length);
   for (var ii = 0; ii < E.length; ii++) {
     var e = E[ii]; // Get one element
     var r = e.getBoundingClientRect(); // Bounding rectangle of element
-
     if ((x > r.left) && (x < r.right) && (y > r.top) && (y < r.bottom)) {
-      console.log("x,y="+x+","+y + " L,R/T,B="+r.left+","+r.right+ "/"+r.top+","+r.bottom);
       Ehits.push(e);
+    //console.log("x,y="+x+","+y + " L,R/T,B="+r.left+","+r.right+ "/"+r.top+","+r.bottom);
     }
-    else { e.style.color = "black"; }
+    else { e.style.color = "black"; } // Revert color once mouse is outside hitbox
   }
-
   return Ehits;
 }
 
